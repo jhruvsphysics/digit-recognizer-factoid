0833ee68ebd93474b0c8de97777c620112254f6a
ruvsphysics <jihosk1995@live.com>
2022-04-05 09:26:25 -0700

    resized and grayscaled image

--- app.py
+++ app.py
@@ -6,41 +6,33 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         image = processb64(request.data)
-        print(image)
         print(image.shape)
         return "success"
     else:
         return render_template('index.html')
 
-from PIL import Image
+from PIL import Image, ImageOps
 import base64
 import io
 import numpy as np
-import torch
 
 def processb64(data):
     print(data)
+    # decode base64 to bytes
     base64_decoded = base64.b64decode(data)
     print(type(base64_decoded))
     print(base64_decoded)
     print(type("cccc"))
     print('=====================================')
     print(io.BytesIO(base64_decoded))
-    # image = Image.Image.frombytes(1, base64_decoded)
-    image = Image.open(io.BytesIO(base64_decoded))
-    image_np = np.array(image)
-    return image_np
 
-# def processb64(data):
-#     print(data)
-#     base64_decoded = base64.b64decode(data)
-#     print(base64_decoded)
-#     # image = Image.open(io.BytesIO(base64_decoded)
-#     print(image)
-#     return true
-#     # image_np = np.array(image)
-#     # image_torch = torch.tensor(np.array(image))
-#     # return image_np
+    # convert to BytesIO, and open via Image.open
+    image = Image.open(io.BytesIO(base64_decoded))
+    image = image.resize((28, 28))
+    # convert to grayscale image
+    gray_image = ImageOps.grayscale(image)
+    image_np = np.array(gray_image)
+    return image_np
 
 
 if __name__ == "__main__":
