b0ab13ade1ff72375abd21c0400fb22203e60e90
ruvsphysics <jihosk1995@live.com>
2022-04-18 11:43:37 -0700

    fix http requests

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect
 from smooth_brain_digit_recognizer import smooth_brain_predict
 from guess_digit import guess_digit
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
